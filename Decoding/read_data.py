daaf93190051f09d8ffffaf66ed396230abe5ccb
senaer <2799280+sena-neuro@users.noreply.github.com>
2021-05-04 21:01:36 +0300

    Decoding test at the bottom deleted

--- Decoding/read_data.py
+++ Decoding/read_data.py
@@ -1,39 +1,35 @@
-import re
 import numpy as np
 import scipy.io
-import scipy.io
 import re
-from pathlib import Path
 from collections import defaultdict
 import pandas as pd
-from itertools import combinations
 
 
-# TODO: HOW TO TEST IF LEAD CONDITON ETC MATCHES
+# TODO: HOW TO TEST IF LEAD CONDITION ETC MATCHES
 def read_data(subject_path):
     # Mapping from condition to label TENTATIVE
-    event_code_to_action_category_map = {
-        "1": "BOD",
-        "4": "OBJ",
-        "7": "PER",
-    }
+    # event_code_to_action_category_map = {
+    #     "1": "BOD",
+    #     "4": "OBJ",
+    #     "7": "PER",
+    # }
 
-    # A dictinary to keep datapoint and labels associated with each lead
+    # A dictionary to keep datapoint and labels associated with each lead
     # Each key is the name of the lead and each value is a dictionary
     # with data and labels. These lists have the same orders (labels are matching the data).
     lead_dict = defaultdict(list)
 
     # Get names of the condition files in the subjecets folder
     condition_paths = [x for x in subject_path.iterdir() if x.match('*.mat')]
-    n_conditions = len(condition_paths)
+    # n_conditions = len(condition_paths)
 
     # Read each condition file and create label
     for condition_path in condition_paths:
 
         condition_mat_struct = scipy.io.loadmat(condition_path, struct_as_record=False)["D"]
-        n_leads = len(condition_mat_struct)
+        # n_leads = len(condition_mat_struct)
 
-        pattern = "condition\_(.*?)\_"
+        pattern = r"condition\_(.*?)\_"
         event_code = re.search(pattern, condition_path.stem).group(1)
 
         # Find the indices of underscore character
@@ -82,7 +78,6 @@ def read_data(subject_path):
                     # since 1 is Body, 4 is OBj and 7 is Person
                     labels.extend([action_category] * no_trials)
 
-
                 else:
 
                     # If there is only one trial then the shape will be 2d,
@@ -91,44 +86,9 @@ def read_data(subject_path):
                     labels.append(action_category)
 
                 # Save the leads data and labels in a map
-                lead_dict["lead"].extend([lead_name]*no_trials)
+                lead_dict["lead"].extend([lead_name] * no_trials)
                 lead_dict["action_category"].extend(labels)
                 lead_dict["power"].extend(data_vectors)
 
     lead_df = pd.DataFrame.from_dict(lead_dict)
     return lead_df
-
-### USAGE ####
-# Using lead data map, we can do a classification for each lead
-#Input path
-input_path = Path().resolve().parent / 'Data' / 'TF_Analyzed'
-
-# Get names of the subject folders
-subject_paths = [x for x in input_path.iterdir() if x.is_dir()]
-n_subjects = len(subject_paths)
-
-lead_df = read_data(subject_paths[0])
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-
-#Classifier is LDA
-clf = LinearDiscriminantAnalysis()
-
-# Decode action categories on each lead
-for lead, data in lead_dict.items():
-  X = data['power']
-  y = data['action_category']
-  print(X.shape)
-  clf.fit(X,y)
-
-conds = ["BOD", "OBJ","PER"]
-
-# Get all trials for a lead by indexing the df
-# for each combination of 2 of condition
-#from itertools import combinations
-
-#comb = combinations([conds, 2)
-
-# Print the obtained combinations
-#for i in list(comb):
-#   do lda and keep the score
-# maybe create a df or dictionary to keep the one vs one scores
