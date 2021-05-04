c82825a721566f9a68238cf11d229ef4f543f36b
huseyinelmas <huseyinorkunelmas@gmail.com>
2021-05-04 15:51:28 +0300

    fix to the 10000,1 problem in dataframes

--- Decoding/read_data.py
+++ Decoding/read_data.py
@@ -1,35 +1,39 @@
+import re
 import numpy as np
 import scipy.io
+import scipy.io
 import re
+from pathlib import Path
 from collections import defaultdict
 import pandas as pd
+from itertools import combinations
 
 
-# TODO: HOW TO TEST IF LEAD CONDITION ETC MATCHES
+# TODO: HOW TO TEST IF LEAD CONDITON ETC MATCHES
 def read_data(subject_path):
     # Mapping from condition to label TENTATIVE
-    # event_code_to_action_category_map = {
-    #     "1": "BOD",
-    #     "4": "OBJ",
-    #     "7": "PER",
-    # }
+    event_code_to_action_category_map = {
+        "1": "BOD",
+        "4": "OBJ",
+        "7": "PER",
+    }
 
-    # A dictionary to keep datapoint and labels associated with each lead
+    # A dictinary to keep datapoint and labels associated with each lead
     # Each key is the name of the lead and each value is a dictionary
     # with data and labels. These lists have the same orders (labels are matching the data).
     lead_dict = defaultdict(list)
 
     # Get names of the condition files in the subjecets folder
     condition_paths = [x for x in subject_path.iterdir() if x.match('*.mat')]
-    # n_conditions = len(condition_paths)
+    n_conditions = len(condition_paths)
 
     # Read each condition file and create label
     for condition_path in condition_paths:
 
         condition_mat_struct = scipy.io.loadmat(condition_path, struct_as_record=False)["D"]
-        # n_leads = len(condition_mat_struct)
+        n_leads = len(condition_mat_struct)
 
-        pattern = r"condition\_(.*?)\_"
+        pattern = "condition\_(.*?)\_"
         event_code = re.search(pattern, condition_path.stem).group(1)
 
         # Find the indices of underscore character
@@ -71,22 +75,25 @@ def read_data(subject_path):
                     # Add the power to the data points list
                     # If there is more than one trial left
                     # then add the trials as list elements
-                    data_vectors.extend(np.hsplit(power.reshape(-1, power.shape[-1]), no_trials))
+                    temp = np.hsplit(power.reshape(-1, power.shape[-1]), no_trials)
 
+                    # Squeeze each trial
+                    for i in range(len(temp)):
+                        temp[i] = temp[i].squeeze()
+
+                    data_vectors.extend(temp)
                     # Add condition as label and add it as many times as the power has trials
                     # We can also use the conditions first character here for the class
                     # since 1 is Body, 4 is OBj and 7 is Person
                     labels.extend([action_category] * no_trials)
-
                 else:
-
                     # If there is only one trial then the shape will be 2d,
                     # add it as one element to the list
                     data_vectors.append(power.flatten())
                     labels.append(action_category)
 
                 # Save the leads data and labels in a map
-                lead_dict["lead"].extend([lead_name] * no_trials)
+                lead_dict["lead"].extend([lead_name]*no_trials)
                 lead_dict["action_category"].extend(labels)
                 lead_dict["power"].extend(data_vectors)
 
