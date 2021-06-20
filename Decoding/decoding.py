f8ffdeb9fd176aed365287b67104243c7c64103a
senaer <2799280+sena-neuro@users.noreply.github.com>
2021-06-20 20:03:00 +0300

    Multiprocessing on server bug is solved. - Sending Pandas DataFrame as parameter was the reason. - DataFrame is converted to array before - Also minor name changes

--- Decoding/decoding.py
+++ Decoding/decoding.py
@@ -5,7 +5,6 @@ Created on Mon May  3 12:42:59 2021
 
 @author: Sena Er github: sena-neuro
 """
-# TODO delete later when we are %100 sure it is not freezing
 from sklearn import svm
 from Decoding.read_data import read_data
 from pathlib import Path
@@ -17,11 +16,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import multiprocessing as mp
+import numpy as np
 
-# TODO LATER For quick Pandas apply
-# import swifter
-
-# Mapping from condition to label TENTATIVE
+# Mapping from condition to label
 event_code_to_action_category_map = {
     "1": "Skin-Displacing",
     "4": "Manipulative",
@@ -29,6 +26,11 @@ event_code_to_action_category_map = {
 }
 action_categories = [ac for ac in event_code_to_action_category_map.values()]
 classification_results_list = []
+
+SUBJECT_NAME_INDEX = 0
+LEAD_INDEX = 1
+ACTION_CLASS_INDEX = 2
+POWER_IDX = 3
 
 
 def binary_action_category_decoder(x, y, cv, clf, grid_search=False, param_grid=None):
@@ -61,31 +63,34 @@ def binary_action_category_decoder(x, y, cv, clf, grid_search=False, param_grid=
     return score, perm_scores, p_value
 
 
-def decode_each_lead(df, lead, subject, cv, clf, grid_search=False, param_grid=None):
+def decode_lead(power_arr, cv, clf, grid_search=False, param_grid=None):
     """
     Pairwise action category decoding on each lead
 
-    :returns decoding_results_dict (Pandas Dataframe)
+    :returns list
         Stores the results of the classification
 
-    :keyword df (Pandas Dataframe)
+    :keyword data_array (nd array)
         Data belonging to a subject
     """
-    df = df.loc[(df['lead'] == lead) &
-                (df['subject_name'] == subject)]
 
     # Run classification for each of the combinations e.g. Body vs Per ...
     for ac_pair in combinations(action_categories, 2):
-        # TODO efficiency
         # Indices of the trials belonging to the first or second action category
-        ind1 = df["action_category"] == ac_pair[0]
-        ind2 = df["action_category"] == ac_pair[1]
+        # Index 2 is the index of action category
+        ind1 = power_arr[:, ACTION_CLASS_INDEX] == ac_pair[0]
+        ind2 = power_arr[:, ACTION_CLASS_INDEX] == ac_pair[1]
+
+        only_ac_pair_arr = power_arr[ind1 | ind2]
 
         # Get the data using the indices (trials with either one label or the other)
-        x = df[ind1 | ind2].power.tolist()
-        y = df[ind1 | ind2].action_category.tolist()
+        x = only_ac_pair_arr[:, POWER_IDX].tolist()             # power
+        y = only_ac_pair_arr[:, ACTION_CLASS_INDEX].tolist()    # action class
 
         score, perm_scores, p_value = binary_action_category_decoder(x, y, cv, clf, grid_search, param_grid)
+
+        subject = power_arr[0, SUBJECT_NAME_INDEX]
+        lead = power_arr[0, LEAD_INDEX]
 
         return [subject,
                 lead,
@@ -104,8 +109,8 @@ def log_result(result):
     print("Lead ", result[1], " is finished!")
 
 
-def mp_decode(df):
-    pool = mp.Pool(1)   # mp.Pool(mp.cpu_count())
+def mp_decode(entire_data_arr):
+    pool = mp.Pool(mp.cpu_count()-1)
 
     # Classifier for the decoding
     clf = make_pipeline(StandardScaler(), svm.SVC())
@@ -122,12 +127,17 @@ def mp_decode(df):
     # We do decoding on each lead of each subject INDEPENDENTLY
     # They can run at the same time; thus, we use multiprocessing
     # We use pool because they can run asynchronously
-    for subject_no, subject in enumerate(df.subject_name.unique()):
-        for lead_no, lead in enumerate(df.lead.unique()):
-            if lead_no < 5:
-                pool.apply_async(decode_each_lead,
-                                 args=(df, lead, subject, cv, clf, True, param_grid),
-                                 callback=log_result)
+
+    all_unique_subjects = np.unique(entire_data_arr[:, SUBJECT_NAME_INDEX])
+    all_unique_leads = np.unique(entire_data_arr[:, LEAD_INDEX])
+    for subject_no, subject in enumerate(all_unique_subjects):
+        for lead_no, lead in enumerate(all_unique_leads):
+            subj_idx = entire_data_arr[:, SUBJECT_NAME_INDEX] == subject
+            lead_idx = entire_data_arr[:, LEAD_INDEX] == lead
+            single_lead_single_subject_arr = entire_data_arr[subj_idx & lead_idx]
+            pool.apply_async(decode_lead,
+                             args=(single_lead_single_subject_arr, cv, clf, True, param_grid),
+                             callback=log_result)
     pool.close()
     pool.join()
 
@@ -148,13 +158,15 @@ if __name__ == '__main__':
         lead_df = read_data(subject_paths[0])
         lead_df.to_pickle(out_file)
 
-    mp_decode(lead_df)
+    lead_arr = lead_df.to_numpy()
+    mp_decode(lead_arr)
 
     classification_results_df = pd.DataFrame.from_records(classification_results_list,
                                                           columns=["subject",
                                                                    "lead",
                                                                    "classification_type",
                                                                    "accuracy",
+                                                                   "permutation scores",
                                                                    "p_value",
                                                                    "n_trials"
                                                                    ])
