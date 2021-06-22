80d4bb57d8893f3cdc40a2e8d61206e7366d25a5
senaer <2799280+sena-neuro@users.noreply.github.com>
2021-06-22 22:50:14 +0300

    Grid search fix - Train/test split was unnecessary - param_grid is fixed (because of pipeline) - cv is now stratified and with 10 splits

--- Decoding/decoding.py
+++ Decoding/decoding.py
@@ -8,12 +8,12 @@ Created on Mon May  3 12:42:59 2021
 from sklearn import svm
 from Decoding.read_data import read_data
 from pathlib import Path
-from sklearn.model_selection import GridSearchCV, train_test_split, KFold, RepeatedKFold, cross_val_score
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
 import pandas as pd
 from itertools import combinations
 from sklearn.model_selection import permutation_test_score
 from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import Pipeline
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import multiprocessing as mp
 import numpy as np
@@ -44,16 +44,13 @@ def binary_action_category_decoder(x, y, cv, clf, grid_search=False, param_grid=
     :keyword y : Any
         action category
     """
-
     if grid_search:
-        # Split into training and testing
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=0)
-
-        search = GridSearchCV(svm.SVC(), param_grid=param_grid, cv=cv, n_jobs=1)
-        search.fit(x_train, y_train)
+        search = GridSearchCV(clf,
+                              param_grid=param_grid,
+                              cv=cv,
+                              n_jobs=1)
+        search.fit(x, y)
         clf = search.best_estimator_
-        x = x_test
-        y = y_test
 
     # Permutation test
     score, perm_scores, p_value = permutation_test_score(
@@ -69,12 +66,14 @@ def decode_lead(power_arr, cv, clf, grid_search=False, param_grid=None):
     :returns list
         Stores the results of the classification
 
-    :keyword data_array (nd array)
+    :keyword power_arr (nd array)
         Data belonging to a subject
     """
+    lead_decoding_results = [None, None, None]
+    subject = power_arr[0, SUBJECT_NAME_INDEX]
+    lead = power_arr[0, LEAD_INDEX]
 
-    lead_decoding_results = []
-
+    ac_pair_idx = 0
     # Run classification for each of the combinations e.g. Body vs Per ...
     for ac_pair in combinations(action_categories, 2):
         # Indices of the trials belonging to the first or second action category
@@ -85,22 +84,20 @@ def decode_lead(power_arr, cv, clf, grid_search=False, param_grid=None):
         only_ac_pair_arr = power_arr[ind1 | ind2]
 
         # Get the data using the indices (trials with either one label or the other)
-        x = only_ac_pair_arr[:, POWER_IDX].tolist()             # power
-        y = only_ac_pair_arr[:, ACTION_CLASS_INDEX].tolist()    # action class
+        x = only_ac_pair_arr[:, POWER_IDX].tolist()  # power
+        y = only_ac_pair_arr[:, ACTION_CLASS_INDEX].tolist()  # action class
 
         score, _, p_value = binary_action_category_decoder(x, y, cv, clf, grid_search, param_grid)
 
-        subject = power_arr[0, SUBJECT_NAME_INDEX]
-        lead = power_arr[0, LEAD_INDEX]
-        res = [subject,
-               lead,
-               ac_pair,
-               score,
-               p_value,
-               len(x)  # number of trials
-               ]
+        lead_decoding_results[ac_pair_idx] = [subject,
+                                              lead,
+                                              ac_pair,
+                                              score,
+                                              p_value,
+                                              len(x)  # number of trials
+                                              ]
+        ac_pair_idx = ac_pair_idx + 1
 
-        lead_decoding_results.append(res)
     return lead_decoding_results
 
 
@@ -108,23 +105,24 @@ def log_result(result):
     # This is called whenever decode_each_lead returns a result.
     # result_list is modified only by the main process, not the pool workers.
     classification_results_list.extend(result)
-    print("Lead ", result[0][1], " is finished!")
 
 
 def mp_decode(entire_data_arr):
     pool = mp.Pool(mp.cpu_count()-1)
 
     # Classifier for the decoding
-    clf = make_pipeline(StandardScaler(), svm.SVC())
+    clf = Pipeline([('scale', StandardScaler()),
+                    ('clf', svm.SVC())])
+    # clf = LinearDiscriminantAnalysis()
 
     # Parameter grid for grid search
-    param_grid = {'C': [0.01, 0.1, 1, 10, 100],
-                  'gamma': [0.001, 0.01, 0.1]}
+    param_grid = {'clf__C': [0.01, 0.1, 1, 10, 100],
+                  'clf__gamma': [0.001, 0.01, 0.1]}
+
+    gs = True
 
     # Create Cross validation
-    cv = KFold(n_splits=5, random_state=0, shuffle=True)
-    # Efficiency: May be too slow and not necessary
-    # cv = RepeatedKFold(n_splits=5, n_repeats=3, random_state=0)
+    cv = StratifiedKFold(n_splits=10, shuffle=True)
 
     # We do decoding on each lead of each subject INDEPENDENTLY
     # They can run at the same time; thus, we use multiprocessing
@@ -138,7 +136,7 @@ def mp_decode(entire_data_arr):
             lead_idx = entire_data_arr[:, LEAD_INDEX] == lead
             single_lead_single_subject_arr = entire_data_arr[subj_idx & lead_idx]
             pool.apply_async(decode_lead,
-                             args=(single_lead_single_subject_arr, cv, clf, True, param_grid),
+                             args=(single_lead_single_subject_arr, cv, clf, gs, param_grid),
                              callback=log_result)
     pool.close()
     pool.join()
@@ -164,12 +162,12 @@ if __name__ == '__main__':
     mp_decode(lead_arr)
 
     classification_results_df = pd.DataFrame.from_records(classification_results_list,
-                                                          columns=["Subject",
-                                                                   "Lead",
-                                                                   "Classification_type",
-                                                                   "Accuracy",
-                                                                   "P_value",
-                                                                   "N_trials"
+                                                          columns=["subject",
+                                                                   "lead",
+                                                                   "classification_type",
+                                                                   "accuracy",
+                                                                   "p_value",
+                                                                   "n_trials"
                                                                    ])
-    classification_results_file = output_path / 'classification_results_df_scale_svm.pkl'
+    classification_results_file = output_path / '22jun_classification_results_svm_GS_final.pkl'
     classification_results_df.to_pickle(str(classification_results_file))
