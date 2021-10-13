5ed3a1e6dd89f285bc92043beccd573c62aa1a8d
valkyrie7 <iamotley7@gmail.com>
2021-10-13 11:17:04 +0300

    All subjects. Flush deleted bcs unnecessary. Prints current subj, condition, lead. (For debugging) Pass already processed subjects.

--- Decoding/read_data.py
+++ Decoding/read_data.py
@@ -19,10 +19,10 @@ parent_path = Path('/auto/data2/oelmas/Intracerebral')
 input_path = parent_path / 'Data' / 'TF_Analyzed'
 output_path = parent_path / 'Data'
 
-power_hdf_file = str(output_path / 'BENEDETTI_TEST_power_data.hdf5')
+power_hdf_file = str(output_path / 'power_data.hdf5')
 
 
-def mat_to_hdf(subject_path: Path):
+def mat_to_hdf(subject_path : Path):
     """
 
     """
@@ -30,63 +30,69 @@ def mat_to_hdf(subject_path: Path):
     # Create and/or open HDF5 in append mode
     file = h5py.File(power_hdf_file, 'a')
     subject_name = subject_path.stem
-    condition_paths = [x for x in subject_path.iterdir() if x.match('*.mat')]
+    
+    if subject_name in file.keys(): # Not tested as of 13 October 2021
+        print(subject_name, " is already in the HDF file. Moving on to the next subject...")
+    else:
+        condition_paths = [x for x in subject_path.iterdir() if x.match('*.mat')]
 
-    for condition_path in condition_paths:
-        condition_mat_struct = scipy.io.loadmat(condition_path, struct_as_record=False)["D"]
+        for condition_path in condition_paths:
+            condition_mat_struct = scipy.io.loadmat(condition_path, struct_as_record=False)["D"]
 
-        ac_code = re.search(r"\d", condition_path.stem).group()
-        action_category = event_code_to_action_category_map[ac_code]
+            ac_code = re.search(r"\d", condition_path.stem).group()
+            action_category = event_code_to_action_category_map[ac_code]
 
-        apostrophe = re.compile("(')")
+            apostrophe = re.compile("(')")
 
-        for idx, lead in enumerate(condition_mat_struct):
+            for idx, lead in enumerate(condition_mat_struct):
 
-            # Get the data in the lead
-            lead_data = lead[0][0][0]
-            # Get lead name
-            lead_name = lead_data.ChanName[0]
-            # Check if the lead name follows the format and isnt one of the unnecessary leads
-            lead_name_correct = re.match("([A-Z]'?(([0-9]+'?)|[A-Z]?)$)", lead_name)
+                # Get the data in the lead
+                lead_data = lead[0][0][0]
+                # Get lead name
+                lead_name = lead_data.ChanName[0]
+                # Check if the lead name follows the format and isnt one of the unnecessary leads
+                lead_name_correct = re.match("([A-Z]'?(([0-9]+'?)|[A-Z]?)$)", lead_name)
 
-            # Check if the lead is iEEG There are some probable unnecesssary channels here e.g EOG2 DEL4?
-            # If the AR_tfX has a size 0f 0 then all trials must have been rejected.
-            if lead_data.ChanType[0] == "iEEG" and lead_name_correct and lead_data.AR_tfX.size != 0:
 
-                # Get the power information (50x200xtrial)
-                power = lead_data.AR_power  # (50, 200, n_trial)
-                # times = lead_data.AR_times[0]  # (200,)
-                # freqs = lead_data.AR_freqs[0]  # (50,)
+                # Check if the lead is iEEG There are some probable unnecesssary channels here e.g EOG2 DEL4?
+                # If the AR_tfX has a size 0f 0 then all trials must have been rejected.
+                if lead_data.ChanType[0] == "iEEG" and lead_name_correct and lead_data.AR_tfX.size != 0:
 
-                # We can't have names with apostrophes so
-                # change the apostrophe with an underscore
-                lead_name_no_ap = apostrophe.sub('_', lead_name)
+                    # Get the power information (50x200xtrial)
+                    power = lead_data.AR_power  # (50, 200, n_trial)
+                    # times = lead_data.AR_times[0]  # (200,)
+                    # freqs = lead_data.AR_freqs[0]  # (50,)
 
-                # Create a HDF5 group with the subject and the lead
-                subject_lead_key = subject_name + '/' + lead_name_no_ap
-                # Open the group if it is not created before create it first
-                group = file.require_group(subject_lead_key)
-                n_trials = power.shape[2] if len(power.shape) == 3 else 1
+                    print(" - ", subject_name, condition_path.stem, lead_name)
 
-                for time_idx in range(200):
-                    power_t = power[:, time_idx].transpose()    # not tested (26 sep)
-                    time_ac_key = 't_' + str(time_idx) + '/' + action_category
+                    # We can't have names with apostrophes so
+                    # change the apostrophe with an underscore
+                    lead_name_no_ap = apostrophe.sub('_', lead_name)
 
-                    # The key is not in the group keys
-                    # First time the key is formed
-                    # Dataset is created
-                    if time_ac_key not in group.keys():
-                        group.create_dataset(time_ac_key,
-                                             data=power_t,
-                                             maxshape=(64, 50)
-                                             )
+                    # Create a HDF5 group with the subject and the lead
+                    subject_lead_key = subject_name + '/' + lead_name_no_ap
+                    # Open the group if it is not created before create it first
+                    group = file.require_group(subject_lead_key)
+                    n_trials = power.shape[2] if len(power.shape) == 3 else 1
 
-                    else:
-                        dataset = group[time_ac_key]
-                        dataset.resize((dataset.shape[1] + n_trials, 50))
-                        dataset[-n_trials:, :] = power_t
+                    for time_idx in range(200):
+                        power_t = power[:, time_idx].transpose()
+                        time_ac_key = 't_' + str(time_idx) + '/' + action_category
 
-    file.flush()
+                        # The key is not in the group keys
+                        # First time the key is formed
+                        # Dataset is created
+                        if time_ac_key not in group.keys():
+                            group.create_dataset(time_ac_key,
+                                                 data=power_t,
+                                                 maxshape=(64, 50)
+                                                 )
+
+                        else:
+                            dataset = group[time_ac_key]
+                            dataset.resize((dataset.shape[1] + n_trials, 50))
+                            dataset[-n_trials:, :] = power_t
+
     file.close()
 
 
@@ -98,5 +104,4 @@ def process_each_subject():
 
 
 if __name__ == '__main__':
-    p = input_path / 'BenedettiL'
-    mat_to_hdf(p)
+    process_each_subject()
