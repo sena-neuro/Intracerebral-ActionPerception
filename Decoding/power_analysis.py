4a3e74f8be42eafe2f0d5e922ea25ef2b32e6e2a
senaer <2799280+sena-neuro@users.noreply.github.com>
2022-02-07 11:25:20 +0300

    - cluster_stat_on_power returns T_obs, H_0 as well, all variables stored as a nested dict access via region and action class - filter_significant_clusters added, needs testing

--- Decoding/power_analysis.py
+++ Decoding/power_analysis.py
@@ -1,42 +1,46 @@
-import h5py
-from pathlib import Path
 import numpy as np
-import pickle
 import numpy.ma as ma
 from mne.stats import permutation_cluster_test
-import hdf_utils
+import hdf_utils as hu
+from collections import defaultdict
 
-hdf = hdf_utils.read_hdf()
-action_classes = ['MN', 'IP', 'SD']
+action_classes = hu.action_classes
+hdf = hu.read_hdf()
 
 
-def cluster_stat_on_power():
-    region_list = hdf_utils.region_list()
-    clusters = dict(fromkeys=region_list)
-    cluster_p_values = dict(fromkeys=region_list)
+def cluster_stat_on_power(region_list=hu.region_list()):
+    nested_dict = lambda: defaultdict(nested_dict)
+    T_obs = nested_dict()
+    clusters = nested_dict()
+    cluster_p_values = nested_dict()
+    H_0 = nested_dict()
 
     for region in region_list:
-        region_leads = hdf_utils.region_to_idx(region)
+        region_leads = hu.region_to_idx(region)
+        arr_shape = (len(region_leads), 200, 50, 64)
+        mean_power = {ac: np.empty(arr_shape, dtype=np.float32) for ac in action_classes}
 
-        mean_power = {ac: np.empty((len(region_leads), 200, 50, 64), dtype=np.float32) for ac in action_classes}
-        clusters[region] = dict(fromkeys=action_classes)
-        cluster_p_values[region] = dict(fromkeys=action_classes)
-
-        #     'MN': np.swapaxes(ma.mean(, axis=0), 0, -1),
-        #     'IP': np.swapaxes(ma.mean(IP_power_dset[region_leads, :, :, :], axis=0), 0, -1),
-        #     'SD': np.swapaxes(ma.mean(SD_power_dset[region_leads, :, :, :], axis=0), 0, -1)
-        # }
-
+        mask_arr = np.empty(arr_shape, dtype='|b1')
+        power_arr = np.empty(arr_shape, dtype=np.float32)
         for ac in action_classes:
-            dset = hdf[ac + '/z_power']
-            dset.read_direct(mean_power[ac], np.s_[region_leads, ...])
-            mean_power[ac] = np.swapaxes(ma.mean(mean_power[ac], axis=0), 0, -1)
+            z_dset = hdf[ac + '/z_power']
+            z_dset.read_direct(power_arr, np.s_[region_leads, ...])
+
+            mask_dset = hdf[ac + '/mask']
+            mask_dset.read_direct(mask_arr, np.s_[region_leads, ...])
+
+            mean_power[ac] = np.swapaxes(ma.mean(ma.masked_array(power_arr, mask=mask_arr), axis=0), 0, -1)
 
         for ac in action_classes:
             other_ac = filter(lambda x: x != ac, action_classes)
             mean_power_rest = np.concatenate((mean_power[next(other_ac)], mean_power[next(other_ac)]))
 
-            _, clusters[ac], cluster_p_values[ac], _ = \
+            T_obs[region][ac], clusters[region][ac], cluster_p_values[region][ac], H_0[region][ac] = \
                 permutation_cluster_test([mean_power[ac], mean_power_rest], n_permutations=1024, tail=1, n_jobs=-1)
+    return T_obs, clusters, cluster_p_values, H_0
 
-    return clusters, cluster_p_values
+# TODO Test
+def filter_significant_clusters(p):
+    if p.any() < 0.05:
+        return True
+    return False
