db7ecc201d2f237f25975e08619f8b84a2e34fe2
senaer <2799280+sena-neuro@users.noreply.github.com>
2022-06-06 10:10:44 +0300

    TFR baseline changed to zscore

--- seeg_action/mass_univariate_analysis.py
+++ seeg_action/mass_univariate_analysis.py
@@ -1,78 +1,15 @@
 import mne
 import numpy as np
-from mne.stats import f_threshold_mway_rm, f_mway_rm
 from mne.time_frequency import tfr_morlet
 import pandas as pd
 from seeg_action import project_config as cfg
+
+
 # pip install git+https://github.com/john-veillette/mne_ari.git
-from mne_ari import all_resolutions_inference
+# from mne_ari import all_resolutions_inference
 
 
-def _prepare_lfp_data(epochs):
-    data = np.array([epochs.get_data(item=condition)[:, 0, :] for condition in epochs.event_id])
-    return data
-
-
-def _prepare_power_data(epochs):
-    freqs = np.geomspace(5, 152, num=50)  # define frequencies of interest
-    n_cycles = freqs / freqs[0]
-
-    epochs_power = list()
-    for condition in [epochs[k] for k in epochs.event_id]:
-        this_tfr = tfr_morlet(condition, freqs, n_cycles=n_cycles,
-                              average=False, return_itc=False, n_jobs=-2)
-        this_tfr.apply_baseline(mode='zlogratio', baseline=(None, 0))
-        this_power = this_tfr.data[:, 0, :, :]  # we only have one channel.
-        epochs_power.append(this_power)
-    return np.asarray(epochs_power)
-
-
-def mass_univariate_rm_anova(epochs, effect, input_type):
-    if effect == 'action_class':
-        effects = 'A'
-    elif effect == 'stimulus_type':
-        effects = 'B'
-    elif effect == 'interaction':
-        effects = 'A:B'
-
-    factor_levels = [3, 3]
-
-    def stat_fun(*args):
-        return f_mway_rm(np.swapaxes(args, 1, 0), factor_levels=factor_levels,
-                         effects=effects, return_pvals=False)[0]
-
-    if input_type == 'lfp':
-        data = _prepare_lfp_data(epochs)
-    elif input_type == 'power':
-        data = _prepare_power_data(epochs)
-
-    # The ANOVA returns a tuple f-values and p-values, we will pick the former.
-    pthresh = 0.05  # set threshold rather high to save some time
-    n_replications = data.shape[1]
-
-    f_thresh = f_threshold_mway_rm(n_replications, factor_levels, effects,
-                                   pthresh)
-    tail = 1  # f-test, so tail > 0
-    n_permutations = 1024
-    F_obs, clusters, cluster_p_values, _ = mne.stats.permutation_cluster_test(
-        data, stat_fun=stat_fun, threshold=f_thresh, tail=tail, n_jobs=-2,
-        n_permutations=n_permutations, buffer_size=1000, out_type='indices')
-
-    return F_obs, clusters, cluster_p_values
-
-
-def regression(epochs, input_type='power', correction='ari'):
-
-    if input_type == 'lfp':
-        data = epochs.get_data()
-    elif input_type == 'power':
-        freqs = np.geomspace(5, 152, num=50)  # define frequencies of interest
-        n_cycles = freqs / freqs[0]
-        epochs_tfr = tfr_morlet(epochs, freqs, n_cycles=n_cycles,
-                              average=False, return_itc=False, n_jobs=-2)
-        epochs_tfr.apply_baseline(mode='zlogratio', baseline=(None, -.2))
-        data = epochs_tfr.data
-
+def regression(epochs, input_type='power', correction='fdr'):
     # Design matrix
     contrasts = ['action_vs_control',
                  'static_vs_dynamic',
@@ -82,85 +19,97 @@ def regression(epochs, input_type='power', correction='ari'):
     design = pd.DataFrame({'condition': np.vectorize(cfg.event_id_to_code.get)(epochs.events[..., -1])})
     design['action_vs_control'] = np.where(design.condition.str.contains('ST'), 2, -3)
     design['static_vs_dynamic'] = np.where(design.condition.str.contains('SC'), 1,  # Static control
-                                            np.where(design.condition.str.contains('DC'), -1,  # Dynamic control
-                                                     0))  # else action stimulus condition
+                                           np.where(design.condition.str.contains('DC'), -1,  # Dynamic control
+                                                    0))  # else action stimulus condition
     design['MN_vs_IP+SD'] = np.where(design.condition.str.contains('MN/ST'), 2,  # Manipulative actions
-                                      np.where(
-                                          (design.condition.str.contains('IP/ST') | design.condition.str.contains('SD/ST')),
-                                          -1,
-                                          0))  # else control stimuli
+                                     np.where(
+                                         (design.condition.str.contains('IP/ST') | design.condition.str.contains(
+                                             'SD/ST')),
+                                         -1,
+                                         0))  # else control stimuli
     design['IP_vs_SD'] = np.where(design.condition.str.contains('IP/ST'), 1,  # Interpersonal actions
-                                   np.where(design.condition.str.contains('SD/ST'), -1,  # Skin-displacing actions
-                                            0))
+                                  np.where(design.condition.str.contains('SD/ST'), -1,  # Skin-displacing actions
+                                           0))
     design.drop(columns='condition', inplace=True)
 
-
-    beta, stderr, t_val, p_val, mlog10_p_val = _regression(data, design, contrasts)
-
-    # Correction
+    # For correction
     alpha = .05
 
-    def regression_stat_fun(data):
-        # TODO dict to ndarray
-        return _regression(data, design, contrasts)[3]
+    for ch in epochs.ch_names:
+        print(ch)
+        ch_epochs = epochs.copy().pick(ch)
+        if input_type == 'lfp':
+            e = ch_epochs
+        elif input_type == 'power':
+            freqs = np.geomspace(5, 152, num=50)  # define frequencies of interest
+            n_cycles = freqs / freqs[0]
+            epochs_tfr = tfr_morlet(ch_epochs, freqs, n_cycles=n_cycles,
+                                    average=False, return_itc=False, n_jobs=-3)
+            epochs_tfr.apply_baseline(mode='zscore', baseline=(-0.4, 0))
+            epochs_tfr.crop(tmin=-0.3, tmax=2.3)
+            e = epochs_tfr
 
-    # fig, ax = plt.subplots()
-    # c = ax.imshow(r2, aspect='auto', origin='lower',
-    #               extent=[times[0], times[-1], freqs[0], freqs[-1]])
-    # ax.set_title('r2')
-    # fig.colorbar(c, ax=ax)
-    # fig.show()
+        times = e.times
+        data = e.data
+        beta, p_val = _regression(data, design, contrasts)
+        #
+        #     def regression_stat_fun(data):
+        #         # TODO dict to ndarray
+        #         return _regression(data, design, contrasts)[3]
+        #
+        #     # fig, ax = plt.subplots()
+        #     # c = ax.imshow(r2, aspect='auto', origin='lower',
+        #     #               extent=[times[0], times[-1], freqs[0], freqs[-1]])
+        #     # ax.set_title('r2')
+        #     # fig.colorbar(c, ax=ax)
+        #     # fig.show()
+        #
+        #     if correction == 'ari':
+        #         # All resolutions interface
+        #         # https://github.com/john-veillette/mne-ari
+        #
+        #         # a function that take a (n_observations, n_tests) array for a one-sample test or
+        #         # and returns an (n_tests, ) long array of p-values
+        #
+        #
+        #         p_vals, tdp, clusters = all_resolutions_inference(data, alpha=alpha,
+        #                                                           n_permutations=100,
+        #                                                           ari_type='permutation',
+        #                                                           statfun=regression_stat_fun
+        #                                                           )
+        #         print('We found %d clusters' % len(clusters))
+        #         return p_vals, tdp, clusters
 
-    if correction == 'ari':
-        # All resolutions interface
-        # https://github.com/john-veillette/mne-ari
-
-        # a function that take a (n_observations, n_tests) array for a one-sample test or
-        # and returns an (n_tests, ) long array of p-values
-
-
-        p_vals, tdp, clusters = all_resolutions_inference(data, alpha=alpha,
-                                                          n_permutations=100,
-                                                          ari_type='permutation',
-                                                          statfun=regression_stat_fun
-                                                          )
-        print('We found %d clusters' % len(clusters))
-        return p_vals, tdp, clusters
-
-    elif correction == 'fdr':
-        import matplotlib.pyplot as plt
-        mask = {}
+        reject_H0 = {}
         pval_corrected = {}
-        times = epochs.times
         for con in contrasts:
-            mask[con], pval_corrected[con] = mne.stats.fdr_correction(p_val[con], alpha=alpha)
-            F_obs_plot2 = beta[con][0].copy()
-            F_obs_plot2[~mask[con][0]] = np.nan
-            fig, ax = plt.subplots(figsize=(6, 4))
-            for f_image, cmap in zip([beta[con][0], F_obs_plot2], ['gray', 'autumn']):
-               c = ax.imshow(f_image, cmap=cmap, aspect='auto', origin='lower',
-                             extent=[times[0], times[-1], freqs[0], freqs[-1]])
-            fig.colorbar(c, ax=ax)
-            ax.set_xlabel('Time (ms)')
-            ax.set_ylabel('Frequency (Hz)')
-            ax.set_title(f'{con} \n'
-                        'FDR corrected (p <= 0.05)')
-            fig.tight_layout()
-            fig.show()
-
-
-
+            reject_H0[con], pval_corrected[con] = mne.stats.fdr_correction(p_val[con], alpha=alpha)
+            if reject_H0[con][0].any():
+                import matplotlib.pyplot as plt
+                F_obs_plot2 = beta[con][0].copy()
+                F_obs_plot2[~reject_H0[con][0]] = np.nan
+                fig, ax = plt.subplots(figsize=(6, 4))
+                for f_image, cmap in zip([beta[con][0], F_obs_plot2], ['gray', 'autumn']):
+                    c = ax.imshow(f_image, cmap=cmap, aspect='auto', origin='lower',
+                                  extent=[times[0], times[-1], freqs[0], freqs[-1]])
+                fig.colorbar(c, ax=ax)
+                ax.set_xlabel('Time (ms)')
+                ax.set_ylabel('Frequency (Hz)')
+                ax.set_title(f'{ch} {con} \n'
+                             'FDR corrected (p <= 0.05)')
+                fig.tight_layout()
+                fig.show()
 
 
 def _regression(data, design_matrix, names):
     from scipy import stats, linalg
 
-    n_samples = len(data)    # 576
-    n_features = np.product(data.shape[1:]) # 3101
+    n_samples = len(data)  # 576
+    n_features = np.product(data.shape[1:])  # 3101
 
-    n_rows, n_predictors = design_matrix.shape # (576, 4)
+    n_rows, n_predictors = design_matrix.shape  # (576, 4)
 
-    y = np.reshape(data, (n_samples, n_features)) # (576, 1, 3101) -> (576, 3101)
+    y = np.reshape(data, (n_samples, n_features))  # (576, 1, 3101) -> (576, 3101)
     betas, resid_sum_squares, _, _ = linalg.lstsq(a=design_matrix, b=y)
 
     df = n_rows - n_predictors
@@ -183,28 +132,81 @@ def _regression(data, design_matrix, names):
         p_val[predictor][stderr_pos] = np.clip((1. - cdf) * 2., tiny, 1.)
         # degenerate cases
         mask = (~stderr_pos & beta_pos)
-        t_val[predictor][mask] = np.inf * np.sign(beta[predictor][mask])
+        # t_val[predictor][mask] = np.inf * np.sign(beta[predictor][mask])
         p_val[predictor][mask] = tiny
         # could do NaN here, but hopefully this is safe enough
         mask = (~stderr_pos & ~beta_pos)
-        t_val[predictor][mask] = 0
+        # t_val[predictor][mask] = 0
         p_val[predictor][mask] = 1.
-        mlog10_p_val[predictor] = -np.log10(p_val[predictor])
 
-    return beta, stderr, t_val, p_val, mlog10_p_val
+    # return beta, stderr, t_val, p_val, mlog10_p_val
+    return beta, p_val
 
 
-def __regression(data, design_matrix, names):
-    from scipy import stats, linalg
+# def __regression(data, design_matrix, names):
+#     from scipy import stats, linalg
+#
+#     n_samples = len(data)    # 576
+#     n_features = np.product(data.shape[1:]) # 3101 or 155050 (3101 * 50) for tfr
+#
+#     n_rows, n_predictors = design_matrix.shape # (576, 4)
+#
+#     shape = (n_samples, n_features)
+#     y = np.reshape(data, shape) # (576, 1, 3101) -> (576, 3101) or (576, 155050)
+#     betas, resid_sum_squares, _, _ = linalg.lstsq(a=design_matrix, b=y)
+#
+#     betas = betas.reshape((n_predictors, data.shape[2], data.shape[3]))
+#     stats.ttest_1samp(betas, popmean=0, alternative='two-sided')
+#     return betas
 
-    n_samples = len(data)    # 576
-    n_features = np.product(data.shape[1:]) # 3101 or 155050 (3101 * 50) for tfr
 
-    n_rows, n_predictors = design_matrix.shape # (576, 4)
+def mass_univariate_rm_anova(epochs, effect, input_type):
+    from mne.stats import f_threshold_mway_rm, f_mway_rm
+    if effect == 'action_class':
+        effects = 'A'
+    elif effect == 'stimulus_type':
+        effects = 'B'
+    elif effect == 'interaction':
+        effects = 'A:B'
 
-    shape = (n_samples, n_features)
-    y = np.reshape(data, shape) # (576, 1, 3101) -> (576, 3101) or (576, 155050)
-    betas, resid_sum_squares, _, _ = linalg.lstsq(a=design_matrix, b=y)
+    factor_levels = [3, 3]
 
-    betas = betas.reshape((n_predictors, data.shape[2], data.shape[3]))
-    return betas
+    def stat_fun(*args):
+        return f_mway_rm(np.swapaxes(args, 1, 0), factor_levels=factor_levels,
+                         effects=effects, return_pvals=False)[0]
+
+    def _prepare_lfp_data(epochs):
+        data = np.array([epochs.get_data(item=condition)[:, 0, :] for condition in epochs.event_id])
+        return data
+
+    def _prepare_power_data(epochs):
+        freqs = np.geomspace(5, 152, num=50)  # define frequencies of interest
+        n_cycles = freqs / freqs[0]
+
+        epochs_power = list()
+        for condition in [epochs[k] for k in epochs.event_id]:
+            this_tfr = tfr_morlet(condition, freqs, n_cycles=n_cycles,
+                                  average=False, return_itc=False, n_jobs=-2)
+            this_tfr.apply_baseline(mode='zscore', baseline=(None, 0))
+            this_power = this_tfr.data[:, 0, :, :]  # we only have one channel.
+            epochs_power.append(this_power)
+        return np.asarray(epochs_power)
+
+    if input_type == 'lfp':
+        data = _prepare_lfp_data(epochs)
+    elif input_type == 'power':
+        data = _prepare_power_data(epochs)
+
+    # The ANOVA returns a tuple f-values and p-values, we will pick the former.
+    pthresh = 0.05  # set threshold rather high to save some time
+    n_replications = data.shape[1]
+
+    f_thresh = f_threshold_mway_rm(n_replications, factor_levels, effects,
+                                   pthresh)
+    tail = 1  # f-test, so tail > 0
+    n_permutations = 1024
+    F_obs, clusters, cluster_p_values, _ = mne.stats.permutation_cluster_test(
+        data, stat_fun=stat_fun, threshold=f_thresh, tail=tail, n_jobs=-2,
+        n_permutations=n_permutations, buffer_size=1000, out_type='indices')
+
+    return F_obs, clusters, cluster_p_values
